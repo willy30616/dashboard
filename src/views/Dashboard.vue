7a45178d24119f56ddd60b2714dc8caf8d5fc48d
Willy <willy30616@gmail.com>
2021-10-20 07:32:57 +0800

    first commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Sidebar />
+    </div>
+</template>
+
+<script>
+import Sidebar from "../components/Sidebar"
+
+export default {
+    name:'Dashboard',
+    components:{ Sidebar }
+}
+</script>
